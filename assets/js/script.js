ec18556aec1937b83d8b21e2a1f893cf74726c87
ChucktownTiger <clemsonalum07@gmail.com>
2022-06-17 21:06:46 -0400

    API pulls to a certain level trying to get it to go further

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,70 @@
-cityform = document.querySelector("#cityinput")
-cityinput = document.querySelector("city")
+var CityForm = document.querySelector('#city-form');
+var CityInput = document.querySelector('#city');
+var cityInfoCont = document.querySelector('#info-container');
+var repoSearchTerm = document.querySelector('#searched-city');
+let urbanareaapi = ""
+var CityFormHandler = function(event) {
+  // prevent page from refreshing
+  event.preventDefault();
 
-var formInputHandler = function(event) {
-    event.preventDefault()
-    var cityname = cityinput.value.trim()
-    if (cityname){
-        getcityinfo(cityname);
-    }
-    else {
-        alert("Please input a city")
-        //make a div to alert they need to input a city
-    }
-}
+  // get value from input element
+  var city = CityInput.value.trim().toLowerCase();
+
+  if (city) {
+    getCityInfo(city);
+
+    // clear old content
+    cityInfoCont.textContent = '';
+    CityInput.value = '';
+  } else {
+    alert('Please enter a City');
+  }
+};
+
+var getCityInfo = function(input) {
+  // format the github api url
+  var apiUrl = 'https://api.teleport.org/api/cities/?search=' + input + '&embed=city%3Asearch-results%2Fcity%3Aitem%2Fcity%3Aurbanarea%2Fua%3Ascores'
+    console.log(apiUrl)
+  // make a get request to url
+  fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        // console.log(apiUrl)
+        // console.log(response);
+        response.json().then(function(data) {
+        //   console.log(data);
+          urbanareaapi = data._embedded["city:search-results"][0]._embedded["city:item"]._links["city:urban_area"]
+          console.log(urbanareaapi)
+        //   displayScore(data, input);
+        });
+      } else {
+        alert('Error: City Not Found');
+      }
+    })
+    .catch(function(error) {
+      alert('Unable to connect to Teleport API');
+    });
+};
+
+console.log(urbanareaapi)
+
+var displayScores = function(scores, searchTerm) {
+  // check if api returned any repos
+  if (scores.length === 0) {
+    cityInfoCont.textContent = 'No City Information found.';
+    return;
+  }
+
+  repoSearchTerm.textContent = searchTerm;
 
 
-var getcityinfo = function(cityinput) {
+};
 
-    var apiURL = 'https://api.teleport.org/api/cities/?search=" + city'
+// add event listeners to forms
+CityForm.addEventListener('submit', CityFormHandler);
 
-    fetch(apiURL)
-        .then(function(response){
-            if (response.ok) {
-                console.log(response)
-                response.json().then(function(data){
-                    console.log(data)
-                    //display city info function
-
-                })
-            } else{
-                alert("city not found")
-            }
-        })
-}
-
-cityinput.addEventListener("submit",formInputHandler);
-
-
-
-
+// _embedded["city:search-results"][0]._embedded["city:item"]._links["city:urban_area"]
 
 // var getCities = function(){
 //     fetch("https://api.teleport.org/api/").then(function(response) {
